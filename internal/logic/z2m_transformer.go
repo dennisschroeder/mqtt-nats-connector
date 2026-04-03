ee089ef4c6bd8e787ad7504ecc4a9f96d59a72f1
Dennis Schröder <dennis.schroeder@deepup.de>
2026-04-03 18:10:38 +0200

    fix(connector): also handle me_doorbell/state topic

--- internal/logic/z2m_transformer.go
+++ internal/logic/z2m_transformer.go
@@ -56,7 +56,7 @@ func (t *Z2MTransformer) TransformMulti(topic string, payload []byte) (string, s
 	}
 
 	// SPECIAL CASE: Doorbell uses /action topic
-	if deviceID == "me_doorbell" && eventType == "action" {
+	if deviceID == "me_doorbell" && (eventType == "action" || eventType == "state") {
 		eventType = "state"
 	}
 
